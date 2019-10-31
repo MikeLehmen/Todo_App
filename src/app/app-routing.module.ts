717b5284f27f99075c45114c74857f7396b2eeeb
MikeLehmen <mike.lehmen@gmail.com>
2019-10-30 20:47:30 -0500

    Switch router module to use HashLocationStrategy so tht routing will work on the dist build.  Normal Angular routing fails on both windows and mac using http-server.  Works not if /#/ is specified.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 
 
@@ -13,6 +14,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 export class AppRoutingModule { }
